b5b7045f6460edde2c66865a3b77b3d3cdfe0e7e
Hope Scott <hopescott@proton.me>
2025-06-18 17:06:29 +0200

    Added Profile Photo

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ const openNav = document.querySelector(".open-menu");
 const closeNav = document.querySelector(".close-menu");
 
 openNav.addEventListener("click", () => {
-    nav.classList.add("menu-toggle");
-    nav.style.transform = "translateX(0%);";
+  nav.classList.add("menu-toggle");
+  nav.style.transform = "translateX(0%);";
 });
 closeNav.addEventListener("click", () => {
-    nav.classList.remove("menu-toggle");
+  nav.classList.remove("menu-toggle");
 });
 
 // FOOTER YEAR
@@ -19,60 +19,44 @@ document.querySelector("footer #year").textContent = footerYear.getFullYear();
 // navLinks
 let navLinks = document.querySelectorAll("nav a");
 const heroSection = document.querySelector("#hero");
+const projectsSection = document.querySelector("#projects");
+const contactSection = document.querySelector("#contact");
 let current = "current";
 
 navLinks.forEach(
-    (c) =>
-        (c.onclick = (e) => {
-            navLinks.forEach((c) =>
-                c.classList[e.target == c ? "toggle" : "remove"](current)
-            );
-            nav.classList.remove("menu-toggle");
+  (c) =>
+    (c.onclick = (e) => {
+      navLinks.forEach((c) =>
+        c.classList[e.target == c ? "toggle" : "remove"](current)
+      );
 
-            let containerId = e.target.textContent;
-            containerId = containerId.toLowerCase();
+      let containerId = e.target.textContent;
+      containerId = containerId.toLowerCase();
 
-            if (containerId === "home") {
-
-                document.querySelector("#hero").style.display = "flex";
-                document.querySelector("#about").style.display = "none";
-                document.querySelector("#blog").style.display = "none";
-                document.querySelector("#resources").style.display = "none";
-                document.querySelector("#contact").style.display = "none";
-                document.querySelector("body").style.background = "#2d2f33";
-            } else if (containerId === "about") {
-                document.querySelector("#hero").style.display = "none";
-                document.querySelector("#about").style.display = "flex";
-                document.querySelector("#blog").style.display = "none";
-                document.querySelector("#resources").style.display = "none";
-                document.querySelector("#contact").style.display = "none";
-                document.querySelector("body").style.background = "#25262a";
-            }else if (containerId === "blog") {
-
-                document.querySelector("#hero").style.display = "none";
-
-                document.querySelector("#about").style.display = "none";
-                document.querySelector("#blog").style.display = "flex";
-                document.querySelector("#resources").style.display = "none";
-                document.querySelector("#contact").style.display = "none";
-                document.querySelector("body").style.background = "#25262a";
-            }else if (containerId === "resources") {
-
-                document.querySelector("#hero").style.display = "none";
-
-                document.querySelector("#about").style.display = "none";
-                document.querySelector("#blog").style.display = "none";document.querySelector("#resources").style.display = "flex";
-                document.querySelector("#contact").style.display = "none";
-                document.querySelector("body").style.background = "#25262a";
-            }
-            else {
-                document.querySelector("#hero").style.display = "none";
-                document.querySelector("#about").style.display = "none";
-                document.querySelector("#blog").style.display = "none";
-
-                document.querySelector("#resources").style.display = "none";
-                document.querySelector("#contact").style.display = "block";
-                document.querySelector("body").style.background = "#25262a";
-            }
-        })
+      if (containerId === "home") {
+        document.querySelector("#hero").style.display = "flex";
+        document.querySelector("#resources").style.display = "none";
+        document.querySelector("#projects").style.display = "none";
+        document.querySelector("#contact").style.display = "none";
+        document.querySelector("body").style.background = "#2d2f33";
+      } else if (containerId === "resources") {
+        document.querySelector("#hero").style.display = "none";
+        document.querySelector("#resources").style.display = "flex";
+        document.querySelector("#projects").style.display = "none";
+        document.querySelector("#contact").style.display = "none";
+        document.querySelector("body").style.background = "#25262a";
+      } else if (containerId === "projects") {
+        document.querySelector("#hero").style.display = "none";
+        document.querySelector("#resources").style.display = "none";
+        document.querySelector("#projects").style.display = "flex";
+        document.querySelector("#contact").style.display = "none";
+        document.querySelector("body").style.background = "#25262a";
+      } else {
+        document.querySelector("#hero").style.display = "none";
+        document.querySelector("#resources").style.display = "none";
+        document.querySelector("#projects").style.display = "none";
+        document.querySelector("#contact").style.display = "block";
+        document.querySelector("body").style.background = "#25262a";
+      }
+    })
 );
